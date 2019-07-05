f03b2f0c021663cf047a2df00eea5a2430149958
Chinh Pham N <chinhpn@bap.jp>
2019-07-05 10:41:30 +0700

    Test incorrect travis

---
+++ SayHello/Views/Controllers/Incorrect/IncorrectViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IncorrectViewController.swift
+//  SayHello
+//
+//  Created by Chinh Pham N. on 7/5/19.
+//  Copyright © 2019 Chính Phạm. All rights reserved.
+//
+
+import UIKit
+
+class IncorrectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // incorrect variable
+        var test = 'travis'
+    }
+}
